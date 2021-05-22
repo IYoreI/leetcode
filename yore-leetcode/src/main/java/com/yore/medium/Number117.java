9a67adcaa33542ef1643c2aa2a03637772a0e047
yore <dy_yore@126.com>
2021-05-22 10:53:40 +0800

    commit 816

--- yore-leetcode/src/main/java/com/yore/medium/Number117.java
+++ yore-leetcode/src/main/java/com/yore/medium/Number117.java
@@ -7,17 +7,12 @@ import java.util.Queue;
 
 /**
  * @author jia bing wen
- * @date 2021/5/17 9:40
+ * @date 2021/5/20 15:04
  * @description
  */
-public class Number116 {
-    /**
-     * 广度优先
-     *
-     * @param root
-     * @return
-     */
-    public Node connect1(Node root) {
+public class Number117 {
+
+    public Node connect(Node root) {
         if (root == null) {
             return null;
         }
@@ -50,27 +45,4 @@ public class Number116 {
         }
         return root;
     }
-
-    /**
-     * 递归
-     */
-    public Node connect(Node root) {
-        if (root == null) {
-            return root;
-        }
-        connectTwoNode(root.left, root.right);
-        return root;
-    }
-
-    public void connectTwoNode(Node node1, Node node2) {
-        if (node1 == null || node2 == null) {
-            return;
-        }
-        node1.next = node2;
-        connectTwoNode(node1.left, node1.right);
-        connectTwoNode(node2.left, node2.right);
-        connectTwoNode(node1.right, node2.left);
-    }
-
-
 }
