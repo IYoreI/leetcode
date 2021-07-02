544e757f2679d852e6226635d40c2280d0d0dd87
yore <dy_yore@126.com>
2021-07-02 10:52:58 +0800

    daily practice 20210702

--- yore-leetcode/src/main/java/com/yore/algorithm/SearchAlgorithm.java
+++ yore-leetcode/src/main/java/com/yore/algorithm/SearchAlgorithm.java
@@ -14,7 +14,8 @@ public class SearchAlgorithm {
         /*
          * 注意循环条件的设置：  <= 或者 <
          * 因为初始化 right 的赋值是 nums.length - 1，即最后一个元素的索引，而不是 nums.length。
-         * 这二者可能出现在不同功能的二分查找中，区别是：前者相当于两端都闭区间 [left, right]，后者相当于左闭右开区间 [left, 		* right)，因为索引大小为 nums.length 是越界的
+         * 这二者可能出现在不同功能的二分查找中，区别是：前者相当于两端都闭区间 [left, right]，
+         * 后者相当于左闭右开区间 [left,right)，因为索引大小为 nums.length 是越界的
          */
         while (left <= right) {
             int mid = left + (right - left) / 2;
@@ -27,6 +28,45 @@ public class SearchAlgorithm {
             }
         }
         return -1;
+    }
+
+    public int leftBound(int[] nums, int target) {
+        if (nums.length == 0) {
+            return -1;
+        }
+        int left = 0;
+        int right = nums.length; // 注意
+
+        while (left < right) { // 注意
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                right = mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid; // 注意
+            }
+        }
+        return left;
+    }
+
+    public int rightBound(int[] nums, int target) {
+        if (nums.length == 0) {
+            return -1;
+        }
+        int left = 0, right = nums.length;
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                left = mid + 1; // 注意
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid;
+            }
+        }
+        return left - 1; // 注意
     }
 
 }
