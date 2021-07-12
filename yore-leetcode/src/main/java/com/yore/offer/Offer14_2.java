28a0b28001e9695e80eee90c8a02377af859bc18
yore <dy_yore@126.com>
2021-07-12 09:47:11 +0800

    daily practice 20210712

--- yore-leetcode/src/main/java/com/yore/offer/Offer14_2.java
+++ yore-leetcode/src/main/java/com/yore/offer/Offer14_2.java
@@ -1,5 +1,8 @@
 package com.yore.offer;
 
+import java.math.BigInteger;
+import java.util.Arrays;
+
 /**
  * @author jia bing wen
  * @date 2021/6/23 10:15
@@ -7,18 +10,14 @@ package com.yore.offer;
  */
 public class Offer14_2 {
     public int cuttingRope(int n) {
-        if (n <= 3) {
-            return n - 1;
+        BigInteger[] dp = new BigInteger[n + 1];
+        Arrays.fill(dp, BigInteger.valueOf(1));
+        for(int i = 3; i < n + 1; i++){
+            for(int j = 1; j < i; j++){
+                dp[i] = dp[i].max(BigInteger.valueOf(j * (i - j))).max(dp[i - j].multiply(BigInteger.valueOf(j)));
+            }
         }
-        int quotient = n / 3;
-        int remainder = n % 3;
-        if (remainder == 0) {
-            return (int) (((long) Math.pow(3, quotient)) % 1000000007);
-        } else if (remainder == 1) {
-            return (int) (((long) Math.pow(3, quotient - 1) * 4) % 1000000007);
-        } else {
-            return (int) (((long) Math.pow(3, quotient) * 2) % 1000000007);
-        }
+        return dp[n].mod(BigInteger.valueOf(1000000007)).intValue();
     }
 
     public static void main(String[] args) {
