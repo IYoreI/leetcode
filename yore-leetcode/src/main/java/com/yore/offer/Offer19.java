dc9b1b2d181c54ec7c34f721b7393032d2c66d14
yore <dy_yore@126.com>
2021-07-16 09:16:10 +0800

    daily practice 20210716

--- yore-leetcode/src/main/java/com/yore/offer/Offer19.java
+++ yore-leetcode/src/main/java/com/yore/offer/Offer19.java
@@ -1,11 +1,13 @@
-package com.yore.hard;
+package com.yore.offer;
+
+import java.util.Stack;
 
 /**
- * @author yore
- * @date 2021/3/28 9:48
+ * @author jia bing wen
+ * @date 2021/7/12 9:15
  * @description
  */
-public class Number10 {
+public class Offer19 {
     public boolean isMatch(String s, String p) {
         int n = s.length();
         int m = p.length();
@@ -37,5 +39,17 @@ public class Number10 {
             }
         }
         return f[n][m];
+    }
+
+    public static void main(String[] args) {
+        Offer19 o = new Offer19();
+        System.out.println(o.isMatch("ab", ".*c"));
+        System.out.println(o.isMatch("aaa", "aaaa"));
+        System.out.println(o.isMatch("aab", "a*c*b"));
+        System.out.println(o.isMatch("ab", ".b"));
+        System.out.println(o.isMatch("ab", ".*"));
+        System.out.println(o.isMatch("aa", "a*"));
+        System.out.println(o.isMatch("aab", "c*a*b"));
+        System.out.println(o.isMatch("mississippi", "mis*is*p*."));
     }
 }
