bf2d0956e052f459bfd9ddca004314c54c6bc298
yore <dy_yore@126.com>
2021-07-19 10:21:05 +0800

    daily practice 20210719

--- yore-leetcode/src/main/java/com/yore/algorithm/SortAlgorithm.java
+++ yore-leetcode/src/main/java/com/yore/algorithm/SortAlgorithm.java
@@ -8,7 +8,8 @@ package com.yore.algorithm;
 public class SortAlgorithm {
     public static void main(String[] args) {
         int[] array = new int[]{1, 8, 4, 2, 6, 4, 9, 3, 5};
-        bubbleSort(array);
+//        bubbleSort(array);
+        mergeSortEnter(array);
         for (int a : array) {
             System.out.print(a + " ");
         }
@@ -31,6 +32,43 @@ public class SortAlgorithm {
                     array[j + 1] = array[j] ^ array[j + 1];
                     array[j] = array[j] ^ array[j + 1];
                 }
+            }
+        }
+    }
+
+
+    /**
+     * 归并排序
+     */
+    static int[] nums, tmp;
+
+    public static void mergeSortEnter(int[] arr) {
+        nums = arr;
+        tmp = new int[nums.length];
+        mergeSort(0, nums.length - 1);
+    }
+
+    public static void mergeSort(int l, int r) {
+        // 终止条件
+        if (l >= r) {
+            return;
+        }
+        // 递归划分
+        int m = (l + r) / 2;
+        mergeSort(l, m);
+        mergeSort(m + 1, r);
+        // 合并阶段
+        int i = l, j = m + 1;
+        for (int k = l; k <= r; k++) {
+            tmp[k] = nums[k];
+        }
+        for (int k = l; k <= r; k++) {
+            if (i == m + 1) {
+                nums[k] = tmp[j++];
+            } else if (j == r + 1 || tmp[i] <= tmp[j]) {
+                nums[k] = tmp[i++];
+            } else {
+                nums[k] = tmp[j++];
             }
         }
     }
