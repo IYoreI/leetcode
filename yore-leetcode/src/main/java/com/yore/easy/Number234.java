3171649fbc2492baa9bb9796de8c235f8955cbc2
yore <dy_yore@126.com>
2021-07-25 14:43:31 +0800

    daily practice 20210725

--- yore-leetcode/src/main/java/com/yore/easy/Number234.java
+++ yore-leetcode/src/main/java/com/yore/easy/Number234.java
@@ -2,6 +2,9 @@ package com.yore.easy;
 
 import com.yore.base.ListNode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author jia bing wen
  * @date 2021/7/20 9:05
@@ -9,7 +12,21 @@ import com.yore.base.ListNode;
  */
 public class Number234 {
     public boolean isPalindrome(ListNode head) {
-
-        return false;
+        List<Integer> list = new ArrayList<>();
+        ListNode node = head;
+        while (node != null) {
+            list.add(node.val);
+            node = node.next;
+        }
+        int begin = 0;
+        int end = list.size() - 1;
+        while (begin <= end) {
+            if (!list.get(begin).equals(list.get(end))) {
+                return false;
+            }
+            begin++;
+            end--;
+        }
+        return true;
     }
 }
