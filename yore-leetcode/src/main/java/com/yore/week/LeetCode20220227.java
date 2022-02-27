d9cba10bda5fecfa10075bc6846657aa021325e2
yore <dy_yore@126.com>
2022-02-27 10:19:21 +0800

    daily practice 20220225

---
+++ yore-leetcode/src/main/java/com/yore/week/LeetCode20220227.java
@@ -0,0 +1,14 @@
+package com.yore.week;
+
+/**
+ * @author Yore
+ * @date 2022/2/27 10:18
+ * @description
+ */
+public class LeetCode20220227 {
+    public static void main(String[] args) {
+        LeetCode20220227 l = new LeetCode20220227();
+    }
+
+
+}
