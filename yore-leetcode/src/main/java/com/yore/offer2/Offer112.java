dc83e30312b5ea9863e8f3a3d30c58daf743a91b
yore <dy_yore@126.com>
2022-03-19 10:34:01 +0800

    daily practice 20220319

--- yore-leetcode/src/main/java/com/yore/offer2/Offer112.java
+++ yore-leetcode/src/main/java/com/yore/offer2/Offer112.java
@@ -1,5 +1,6 @@
 package com.yore.offer2;
 
+import java.util.Arrays;
 import java.util.Deque;
 import java.util.LinkedList;
 
@@ -9,36 +10,36 @@ import java.util.LinkedList;
  * @description
  */
 public class Offer112 {
+    public static void main(String[] args) {
+        Offer112 o = new Offer112();
+        System.out.println(o.longestIncreasingPath(new int[][]{{9, 9, 4}, {6, 6, 8}, {2, 1, 1}}));
+    }
+
     public int longestIncreasingPath(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
         int[][] dp = new int[m][n];
         int[][] op = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-        int x = 0, y = 0;
-        int min = Integer.MAX_VALUE;
+        for (int[] a : dp) {
+            Arrays.fill(a, 1);
+        }
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                dp[i][j] = 1;
-                if (min > matrix[i][j]) {
-                    min = matrix[i][j];
-                    x = i;
-                    y = j;
-                }
-            }
-        }
-        Deque<int[]> queue = new LinkedList<>();
-        queue.offerLast(new int[]{x, y});
-        while (!queue.isEmpty()) {
-            int[] a = queue.pollFirst();
-            x = a[0];
-            y = a[1];
-            for (int i = 0; i < 4; i++) {
-                int r = x - op[i][0];
-                int c = y - op[i][1];
-                if (r >= 0 && r < m && c >= 0 && c < n) {
-                    if (matrix[r][c] > matrix[x][y]) {
-                        dp[r][c] += 1;
-                        queue.offerLast(new int[]{r, c});
+                Deque<int[]> queue = new LinkedList<>();
+                queue.offerLast(new int[]{i, j});
+                while (!queue.isEmpty()) {
+                    int[] a = queue.pollFirst();
+                    int x = a[0];
+                    int y = a[1];
+                    for (int k = 0; k < 4; k++) {
+                        int r = x - op[k][0];
+                        int c = y - op[k][1];
+                        if (r >= 0 && r < m && c >= 0 && c < n) {
+                            if (matrix[r][c] > matrix[x][y] && dp[r][c] < dp[x][y] + 1) {
+                                dp[r][c] = dp[x][y] + 1;
+                                queue.offerLast(new int[]{r, c});
+                            }
+                        }
                     }
                 }
             }
