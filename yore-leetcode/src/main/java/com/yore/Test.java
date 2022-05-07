a79250b131faa4effa6b8de4e0473af16786aa90
yore <dy_yore@126.com>
2022-05-07 11:06:05 +0800

    daily practice 20220507

--- yore-leetcode/src/main/java/com/yore/Test.java
+++ yore-leetcode/src/main/java/com/yore/Test.java
@@ -1,6 +1,6 @@
 package com.yore;
 
-import java.security.SecureRandom;
+import java.util.concurrent.*;
 
 /**
  * @author Yore
@@ -9,11 +9,30 @@ import java.security.SecureRandom;
  */
 public class Test {
 
-    public static void main(String[] args) {
-        SecureRandom random = new SecureRandom();
-        for(int i=0;i<10;i++){
-            int number = random.nextInt(10);
-            System.out.println(number);
-        }
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        ThreadFactory threadFactory = Executors.defaultThreadFactory();
+
+        ExecutorService executorService1 = new ThreadPoolExecutor(5,5,0L,
+                TimeUnit.MICROSECONDS,new LinkedBlockingQueue<>(1024),threadFactory);
+        Callable<String> c = new MyCallable("1");
+        Future<String> f = executorService1.submit(c);
+        String res = f.get();
+        System.out.println(res);
+        System.out.println("end");
+        executorService.shutdown();
+    }
+}
+
+class MyCallable implements Callable<String> {
+    private String oid;
+
+    public MyCallable(String oid) {
+        this.oid = oid;
+    }
+
+    @Override
+    public String call() throws Exception {
+        return oid;
     }
 }
